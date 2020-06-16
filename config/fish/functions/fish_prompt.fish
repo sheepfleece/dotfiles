d26c0a4b889b6cd1b09dbff15534f413ec125f79
sheepfleece <rowanmorrison@protonmail.ch>
2020-06-16 10:48:51 +0300

    Visually distinguish recursively open shells

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -5,16 +5,26 @@ function fish_prompt
   end
   if jobs -q
     set_color e27878
-    echo -n '★'
+    prompt_echo '★'
   end
   if test "$IN_NIX_SHELL" = "impure" 
     set_color 84a0c6
-    echo -n 'λ '
+    prompt_echo 'λ'
   else if test "IN_NIX_SHELL" = "pure"
     set_color e27878
-    echo -n 'λ '
+    prompt_echo 'λ'
   else 
     set_color normal
-    echo '$ '
+    prompt_echo '$'
   end
 end
+
+function echo_replicate --description "Echo the character for each running instance of a shell"  
+  echo -n (seq 1 (ps | grep fish | wc -l) | tr -d '\n' | sed "s/[0-9]/$argv/g")
+end
+
+function prompt_echo --description "Echo the character and append <space> to it"
+  echo_replicate $argv
+  echo -n ' '
+end
+
